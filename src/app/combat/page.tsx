91e256b0c3fc7671934524cb552e2a16efa5bd0e
Bailey Thorp <bailey.thorp@techtorium.ac.nz>
2025-11-10 12:48:58 +1300

    some combat UI

--- src/app/combat/page.tsx
+++ src/app/combat/page.tsx
@@ -2,7 +2,8 @@
 
 import { type } from "os";
 import { useState } from "react";
-import { Game, Player, Enemy } from "@/classes/gameClasses"
+import { Game, Player, Enemy } from "@/classes/gameClasses";
+import Image from "next/image";
 
 export default function Menu() {
 
@@ -12,6 +13,8 @@ export default function Menu() {
   const [ game, setGame ]  = useState<Game>(new Game(p, es))
 
   function doPlayerTurn(): void {
+    //inputs: target, dmg, status, etc. default set to none.
+    // takedamage function
       
       let ng = game.clone()
       ng.player.attack(game.enemies[0]) //this reduce's enemy.hp by 1
@@ -19,12 +22,49 @@ export default function Menu() {
     }
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen p-8">
-        <h1 className="text-3xl font-bold mb-4">Combat</h1>
-        <h1 className="font-bold mb-4">player hp: {game.player.hp}</h1>
-        <h1 className="font-bold mb-4">player sp: {game.player.sp}</h1>
-        <h1 className="font-bold mb-4">enemy hp: {game.enemies[0].hp}</h1>
-        <div onClick={() => doPlayerTurn()}>Attack</div>
+    <div className="container border mx-auto">
+      <div className="grid grid-cols-5 border border-red-500">
+
+        {/* This player/enemy section needs to be made into a component */}
+        <div className="flex flex-col items-center">
+          <div>statuses</div>
+          <Image
+            src="/player.png"
+            alt="player"
+            width={160}
+            height={160}
+          />
+
+          <div>name</div>
+          <div>hp/sp</div>
+        </div>
+
+        <div className="flex flex-col items-center">
+          <div>statuses</div>
+          <Image
+            src="/enemy.png"
+            alt="player"
+            width={160}
+            height={160}
+          />
+
+          <div>name</div>
+          <div>hp/sp</div>
+        </div>
+        
+        <div className="">asd</div>
+        <div className="">asd</div>
+        <div className="">asd</div>
+
+      </div>
+      <div className="flex flex-col items-center justify-center p-8">
+          <h1 className="text-3xl font-bold mb-4">Combat</h1>
+          <h1 className="font-bold mb-4">player hp: {game.player.hp}</h1>
+          <h1 className="font-bold mb-4">player sp: {game.player.sp}</h1>
+          <h1 className="font-bold mb-4">enemy hp: {game.enemies[0].hp}</h1>
+          <div onClick={() => doPlayerTurn()}>Attack</div>
+      </div>
     </div>
+      
   );
 }
