1c13026a3e6d89ba31bb04bda79dcb898da55d16
Bailey Thorp <bailey.thorp@techtorium.ac.nz>
2025-11-11 11:50:49 +1300

    display cards differently on combat

--- src/app/combat/page.tsx
+++ src/app/combat/page.tsx
@@ -1,8 +1,9 @@
 "use client";
 
 import { type } from "os";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Game, Player, Enemy } from "@/static/gameClasses";
+import { DEFAULT_CARDS } from "@/static/Cards";
 import Entity from "@/components/entity";
 import Image from "next/image";
 import { useCardSelection } from "@/context/CardSelectionContext";
@@ -10,6 +11,7 @@ import { useCardSelection } from "@/context/CardSelectionContext";
 export default function Combat() {
   const { selectedCards, setSelectedCards } = useCardSelection();
   
+  useEffect(() => {setSelectedCards(selectedCards.concat(DEFAULT_CARDS))}, [])
 
   //const { game, doPlayerTurn } = useGame()
   let p = new Player(10, 5)
@@ -29,32 +31,31 @@ export default function Combat() {
   return (
     <div className="container border mx-auto">
       <div className="grid grid-cols-5 border border-red-500">
-
         <Entity name="player" isPlayer={true} hp={game.player.hp} sp={game.player.sp} />
         {game.enemies.map(enemy =>
           <Entity key={enemy.id} name="enemy1" isPlayer={false} hp={enemy.hp} />
         )}
-
-        {/* map each active turn card to a button. each card does it effect using the game/player */}
-
-
       </div>
+
       <div className="flex flex-col items-center justify-center p-8">
           <div className="cursor-pointer text-3xl" onClick={() => action()}>Attack</div>
       </div>
-      {selectedCards.map((card) => (
-        <div key={card.id} className={`w-full grid grid-cols-2 border-b p-2 cursor-pointer`}>
-          <div className="flex flex-row gap-2 items-center">
-            <Image
-              src={"/cards/" + card.icon}
-              alt={card.name + " icon"}
-              width={50}
-              height={50}
-            />
-            <div>{card.name}</div>
+
+      <div className="grid grid-cols-5 gap-4 items-center w-fit">
+
+        {selectedCards.map((card) => (
+          <div key={card.id} className={`w-full cursor-pointer`}>
+            <div className="">
+              <Image
+                src={"/cards/" + card.icon}
+                alt={card.name + " icon"}
+                width={80}
+                height={80}
+                />
+            </div>
           </div>
-        </div>
         ))}
+      </div>
     </div>
       
   );
